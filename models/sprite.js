3a04be46c55fdd9b08c8e5b1f4d7143cd33a4e4c
Nick Doiron <nicholas.doiron@asiafoundation.org>
2016-02-22 10:04:06 -0800

    create a bigger SpriteAtlas for halfsprite

--- models/sprite.js
+++ models/sprite.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 var spriteSchema = mongoose.Schema({
   name: String,
-  images: [String]
+  images: [Object],
+  width: Number,
+  height: Number
 });
 
 module.exports = mongoose.model('Sprite', spriteSchema);
--- static/map.js
+++ static/map.js
@@ -2,7 +2,7 @@ $(function() {
   mapboxgl.accessToken = "pk.eyJ1IjoibWFwbWVsZCIsImEiOiI0a1NzYW53In0.2gQTd6k9Ghw8UBK4DsciLA";
   var map = new mapboxgl.Map({
     container: 'map',
-    center: [42.786, -73.26],
+    center: [-73.26, 42.786],
     zoom: 6,
     style: {
       "version": 8,
